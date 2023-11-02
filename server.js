e3038d49af283d8eede5729f97270b3e0c619e4b
turtletowerz <turtletowerz@proton.me>
2023-11-01 21:38:47 -0400

    change file structure and add playlist rendering

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var app = express();
 app.set('view engine', 'ejs');
 
 // Serve static files from the 'css', 'js', and 'img' directory
-app.use(express.static('css'));
-app.use(express.static('js'));
-app.use(express.static('img'));
+app.use(express.static('static/css'));
+app.use(express.static('static/js'));
+app.use(express.static('static/img'));
 
 // use res.render to load up an ejs view file
 
@@ -16,5 +16,29 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 });
 
+class Song {
+  constructor(artist, title, length) {
+    this.artist = artist
+    this.title = title
+    this.length = length
+  }
+}
+
+class Playlist {
+  constructor(playlist, dj, songs) {
+    this.playlist = playlist
+    this.dj = dj
+    this.songs = songs
+  }
+}
+
+app.get('/playlists', function(req, res) {
+  res.render('pages/playlists');
+});
+
+app.get('/playlist', function(req, res) {
+  res.render('pages/playlists');
+});
+
 app.listen(8080);
-console.log('Server is listening on port 8080');
+console.log('Server is listening on 127.0.0.1:8080');
