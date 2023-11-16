52f3cb6d34ea2ccbaad75add041c11a5e4252e30
KarthikeyanKrishnanGMU <112577805+KarthikeyanKrishnanGMU@users.noreply.github.com>
2023-11-16 02:02:32 -0500

    Updated userPreferences.js

--- KKNodeJS/public/js/userPreferences.js
+++ KKNodeJS/public/js/userPreferences.js
@@ -12,6 +12,7 @@ function genreSuggestionForm() {
 
 function artistSuggestionForm() {
     let y = document.forms["artistForm"]["artist"].value;
+
     if (y == " ") {
         alert("Error! DJ artist must be filled out.");
         return false;
@@ -22,9 +23,32 @@ function artistSuggestionForm() {
     }
 }
 
+let userPreferences =
+{
+  favGenres: "",
+  favArtists: "",
+};
+
 function nextFunction() 
 {
   alert("\'Next\' Button has been clicked");
   console.log(userPreferences.favGenres);
   console.log(userPreferences.favArtists);
 }
+
+document.getElementById("nextBtn").addEventListener("click", nextFunction);
+
+function genreFunc(genre) {
+  var btnVal = genre.innerText;
+  alert(btnVal + ' has been added to your preferences.');
+  userPreferences.favGenres = userPreferences.favGenres + genre.innerText + " ";
+  console.log(btnVal);  
+}
+
+function artistFunc(artist) {
+  var btnVal2 = artist.innerText;
+  alert(btnVal2 + ' has been added to your preferences.');
+  userPreferences.favArtists = userPreferences.favArtists + artist.innerText + " ";
+  console.log(btnVal2);
+}
+
