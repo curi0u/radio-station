f4d8be5bcb982fed82dd0002337b60475ffba46a
KarthikeyanKrishnanGMU <112577805+KarthikeyanKrishnanGMU@users.noreply.github.com>
2023-11-29 17:33:37 -0500

    Updated userPreferences.js

--- static/js/userPreferences.js
+++ static/js/userPreferences.js
@@ -22,9 +22,31 @@ function artistSuggestionForm() {
     }
 }
 
+let userPreferences =
+{
+  favGenres: "",
+  favArtists: "",
+};
+
 function nextFunction() 
 {
   alert("\'Next\' Button has been clicked");
   console.log(userPreferences.favGenres);
   console.log(userPreferences.favArtists);
+}
+
+document.getElementById("nextBtn").addEventListener("click", nextFunction);
+
+function genreFunc(genre) {
+  var btnVal = genre.innerText;
+  alert(btnVal + ' Button has been clicked');
+  userPreferences.favGenres = userPreferences.favGenres + genre.innerText + " ";
+  console.log(btnVal);
+}
+
+function artistFunc(artist) {
+  var btnVal2 = artist.innerText;
+  alert(btnVal2 + ' Button has been clicked');
+  userPreferences.favArtists = userPreferences.favArtists + artist.innerText + " ";
+  console.log(btnVal2);
 }
